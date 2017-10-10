3674c8f50eeedb616fa1b0921c49261f88c1c5a3
priyanka nilesh bongale <pb435@afsconnect1.njit.edu>
2017-10-09 22:36:43 -0400

    Webpage

---
+++ webpage.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Hello Everyone</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+	<h1>Welcome to Priyanka Bongale's Webpage</h1>
+			
+        <h1>I am pursuing Masters in Information Systems from New Jersey Institute of Technology</h2>
+                   <h3>I have 3 years experience working as a QA Analyst at Vistex Asia Pacific Ltd, India </h3>
+                   <h3>I like the subject Web Systems Development</h4>
+                   <p>I want to become one of the leading Business Analysts in future </p>
+		<p>My hobbies are:</p>
+                                  <ul style="margin-top:10px;">
+					
+			          <li>1. Listening to songs</li>
+				  <li>2. Exploring new places</li>
+				  <li>3. Playing Badminton </li>
+                                 
+					
+				</ul>
+			
+			
+			
+
+</body>
+</html>
